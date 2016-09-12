9eb7b360d72745f6673a3d4bbe3b766f1e06702f
Tony Chen <tchen@mbp15-fwg8wp.8x8.com>
2016-09-12 11:21:54 -0700

    8x8 Phone Dialer

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,14 @@ var webpack = require('webpack');
 module.exports = {
   entry: './index.js',
   output: { path: __dirname, filename: 'bundle.js' },
+  // Node Production environment
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    })
+  ],
   module: {
     loaders: [
       {
