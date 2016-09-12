10c3205225581b3223fa4be3fc6088914dbb856c
Tony Chen <tchen@mbp15-fwg8wp.8x8.com>
2016-09-12 11:55:14 -0700

    8x8 Phone Dialer

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import React, { Component, PropTypes } from 'react';
 // import ReactDOM from 'react-dom';
 
-class PhoneDialer extends React.Component {
+export default class PhoneDialer extends React.Component {
   render() {
     return(
       <div>Hello World</div>
