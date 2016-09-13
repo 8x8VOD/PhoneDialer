bfadbf0038800c53a1c012f3c9851cfe7a93062c
Tony Chen <tchen@MBP15-FWG8WP.local>
2016-09-13 00:51:26 -0700

    8x8 Phone Dialer

--- webpack.config.js
+++ webpack.config.js
@@ -6,62 +6,83 @@ var isDevelopment = process.env.NODE_ENV !== 'production';
 var BUILD_PATH = path.resolve(__dirname, 'lib');
 
 var common = {
-  entry: {},
-  output: {},
-  module: {
-    loaders: [
-      { test: /\.js$/, exclude: /node_modules/, loader: 'babel' },
-      { test: /\.css$/, exclude: /node_modules/, loader: 'style!css' },
-      { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&mimetype=application/font-woff" },
-      { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" }
+        entry: {},
+        output: {},
+        module: {
+            loaders: [{
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel'
+            }, {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                loader: 'style!css'
+            }, {
+                test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
+                loader: "url?limit=10000&mimetype=application/font-woff"
+            }, {
+                test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
+                loader: "url?limit=10000&mimetype=application/font-woff"
+            }, {
+                test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+                loader: "url?limit=10000&mimetype=application/octet-stream"
+            }, {
+                test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+                loader: "file"
+            }, {
+                test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+                loader: "url?limit=10000&mimetype=image/svg+xml"
+            }
+
+        ]
+    },
+    plugins: [
+        new webpack.NoErrorsPlugin()
     ]
-  },
-  plugins: [
-    new webpack.NoErrorsPlugin()
-  ]
 };
 
 var finalConf;
 if (isDevelopment) {
-  finalConf = Object.assign({}, common);
-  finalConf.entry.app = ['webpack-hot-middleware/client', './demos/index.js']; // HMR
-  finalConf.output = {
-    path: BUILD_PATH,
-    filename: 'demo.js'
-  };
-  finalConf.devtool = 'source-map';
-  finalConf.plugins = [...finalConf.plugins,
-    new HtmlWebpackPlugin({
-      template: './index.html'
-    }),
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.HotModuleReplacementPlugin()];
+    finalConf = Object.assign({}, common);
+    finalConf.entry.app = ['webpack-hot-middleware/client', './demos/index.js']; // HMR
+    finalConf.output = {
+        path: BUILD_PATH,
+        filename: 'demo.js'
+    };
+    finalConf.devtool = 'source-map';
+    finalConf.plugins = [...finalConf.plugins,
+        new HtmlWebpackPlugin({
+            template: './index.html'
+        }),
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.HotModuleReplacementPlugin()
+    ];
 } else {
-  // production config
-  finalConf = Object.assign({}, common);
-  finalConf.entry.app = './app/index.js';
-  finalConf.output = {
-    path: BUILD_PATH,
-    filename: 'index.js',
-    sourceMapFilename: 'index.map',
-    library: 'InfiniteScroll',
-    libraryTarget: 'umd'
-  };
-  finalConf.externals = {
-    'react': {
-      root: 'React',
-      commonjs2: 'react',
-      commonjs: 'react',
-      amd: 'react'
-    }
-  };
-  finalConf.plugins = [...finalConf.plugins,
-    new webpack.optimize.OccurrenceOrderPlugin(),
-    new webpack.optimize.DedupePlugin(),
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': '"production"'
-    })
-  ];
+    // production config
+    finalConf = Object.assign({}, common);
+    finalConf.entry.app = './app/index.js';
+    finalConf.output = {
+        path: BUILD_PATH,
+        filename: 'index.js',
+        sourceMapFilename: 'index.map',
+        library: 'InfiniteScroll',
+        libraryTarget: 'umd'
+    };
+    finalConf.externals = {
+        'react': {
+            root: 'React',
+            commonjs2: 'react',
+            commonjs: 'react',
+            amd: 'react'
+        }
+    };
+    finalConf.plugins = [...finalConf.plugins,
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        new webpack.optimize.DedupePlugin(),
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': '"production"'
+        })
+    ];
 }
 
 module.exports = finalConf;
